d885610befd587a1f080dcbdff6717baadf3fa05
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-11-18 17:40:55 +0100

    added session

--- server/app.js
+++ server/app.js
@@ -2,12 +2,21 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv/config";
+import session from "express-session";
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 const users = [{ id: 1, name: "Mo" }];
 
@@ -15,6 +24,5 @@ app.get("/", (req, res) => {
   res.status(200).send({ data: users });
 });
 
-console.log(process.env.SESSION_SECRET);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server is running on port:", PORT));
