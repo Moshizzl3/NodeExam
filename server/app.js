c2c918d7a33e7ec4297091ace1e96f623a7c72f5
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-11-18 16:58:46 +0100

    testing env file

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv/config";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
+const users = [{ id: 1, name: "Mo" }];
 
+app.get("/", (req, res) => {
+  res.status(200).send({ data: users });
+});
 
-
-
-
-
-
-
-
+console.log(process.env.SESSION_SECRET);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server is running on port:", PORT));
